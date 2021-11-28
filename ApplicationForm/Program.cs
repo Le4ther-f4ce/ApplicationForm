9b52f695169d45bd86db990430ffc86c7e62fba2
Алексей Федюшин <alexeyfedyshinural@gmail.com>
2021-11-28 17:37:18 +0500

    Добавьте файлы проекта.

---
+++ ApplicationForm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ApplicationForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, как вас зовут? ");
+            string userName = Console.ReadLine();
+            
+            Console.WriteLine("Сколько вам лет? ");
+            byte userAge = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Какое у вас хобби?");
+            string userHobby = Console.ReadLine();
+
+            Console.WriteLine($"\nВас зовут: {userName}\n" +
+                $"Ваш возраст: {userAge}\n" +
+                $"Вы увлекаетесь: {userHobby}");
+        }
+    }
+}
